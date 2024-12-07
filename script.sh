ff91a5c9bf3f8e486536897550edef5ff54185d9
hnle <harrison.le@paradigm.co>
2024-12-07 13:14:18 -0800

    Remove cookie

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ if [ $# -lt 1 ]; then
     echo "Usage: $0 <day>"
     exit 1
 fi
-COOKIE=53616c7465645f5f24816c25af32156af459144ca54d83e231c455e7a73bf121023426ee1c597c7e76bb3c8decd670805bc35630ca390e82aa4a218731476134
+COOKIE=
 DAY=$1
 INPUT="./$DAY/input.txt"
 PY="./$DAY/day$DAY.py"
